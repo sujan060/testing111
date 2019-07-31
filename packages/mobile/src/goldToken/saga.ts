0c88e0e02635406e5584cffe8775495a1b8e7c55
annakaz <annakaz@users.noreply.github.com>
2019-07-31 09:03:34 +0800

    Add gold token to goldToken saga

--- packages/mobile/src/goldToken/saga.ts
+++ packages/mobile/src/goldToken/saga.ts
@@ -1,17 +1,44 @@
 import { getGoldTokenContract } from '@celo/contractkit'
-import { spawn } from 'redux-saga/effects'
+import BigNumber from 'bignumber.js'
+import { call, put, spawn } from 'redux-saga/effects'
 import { CURRENCY_ENUM } from 'src/geth/consts'
 import { Actions, fetchGoldBalance, setBalance } from 'src/goldToken/actions'
-import { tokenFetchFactory, tokenTransferFactory } from 'src/tokens/saga'
+import { tokenTransferFactory } from 'src/tokens/saga'
+import Logger from 'src/utils/Logger'
+import { getConnectedAccount } from 'src/web3/saga'
 
 const tag = 'goldToken/saga'
 
-export const goldFetch = tokenFetchFactory({
-  actionName: Actions.FETCH_BALANCE,
-  contractGetter: getGoldTokenContract,
-  actionCreator: setBalance,
-  tag,
-})
+export async function getGoldTokenBalance() {
+  const account = await getConnectedAccount()
+  fetch(
+    `http://alfajores-blockscout.celo-testnet.org/api?module=account&action=balance&address=${account}`,
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+    .then((response) => {
+      return response.json()
+    })
+    .then((responseJson) => {
+      const balance = new BigNumber(responseJson.result).times(1e-19)
+      Logger.debug('@getGoldTokenBalance', `Got balance of ${balance}$`)
+      return balance
+    })
+    .catch((error) => {
+      Logger.error('@getGoldTokenBalance', 'Failed to fetch gold token balance', error)
+    })
+}
+
+export function* goldFetch() {
+  const balance: BigNumber = yield call(getGoldTokenBalance)
+  Logger.debug('@stableTokenFetch', balance.toString())
+  yield put(setBalance(balance.toString()))
+}
 
 export const goldTransfer = tokenTransferFactory({
   actionName: Actions.TRANSFER,
