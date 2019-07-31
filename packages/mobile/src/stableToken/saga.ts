61e63349beac893996e488895adff900d4003ca8
annakaz <annakaz@users.noreply.github.com>
2019-07-31 08:30:12 +0800

    Call blockchain API wip

--- packages/mobile/src/stableToken/saga.ts
+++ packages/mobile/src/stableToken/saga.ts
@@ -1,17 +1,36 @@
 import { getStableTokenContract } from '@celo/contractkit'
-import { spawn } from 'redux-saga/effects'
+import { call, spawn } from 'redux-saga/effects'
 import { CURRENCY_ENUM } from 'src/geth/consts'
 import { Actions, fetchDollarBalance, setBalance } from 'src/stableToken/actions'
-import { tokenFetchFactory, tokenTransferFactory } from 'src/tokens/saga'
+import { tokenTransferFactory } from 'src/tokens/saga'
+import Logger from 'src/utils/Logger'
 
 const tag = 'stableToken/saga'
 
-export const stableTokenFetch = tokenFetchFactory({
+export function* stableTokenFetch() {
+  const balance: any = yield call(
+    fetch,
+    'https://alfajores-blockscout.celo-testnet.org/api?module=account&action=balance&address=0xe167029b1a56c6cba74c35c472e9db3c9e2402ff',
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+    }
+  )
+  Logger.debug('@stableTokenFetch', balance)
+  // Logger.debug('@stableTokenFetch', balance.json().stringify())
+  Logger.debug('@stableTokenFetch', JSON.stringify(balance.json()))
+}
+
+/*tokenFetchFactory({
   actionName: Actions.FETCH_BALANCE,
   contractGetter: getStableTokenContract,
   actionCreator: setBalance,
   tag,
 })
+*/
 
 export const stableTokenTransfer = tokenTransferFactory({
   actionName: Actions.TRANSFER,
