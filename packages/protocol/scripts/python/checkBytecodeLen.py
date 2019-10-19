a2302567266014397f24b324fa24b1eed4d81142
yorhodes <yorkerhodesiv@gmail.com>
2019-10-19 04:57:41 -0500

    Improve bytecode len checker with colors and close metric

--- packages/protocol/scripts/python/checkBytecodeLen.py
+++ packages/protocol/scripts/python/checkBytecodeLen.py
@@ -3,16 +3,40 @@
 import json
 import os
 
-buildPath = "./build/contracts"
+from termcolor import colored
 
-bytecodeLimit = 2**15 + 2**14 # note: doubled from Ethereum's bytecode limit
+bytecodeLimit = 2**15 + 2**14 # doubled from Ethereum's bytecode limit
+closeDifferential = 2**14
+
 print("Limit: " + str(bytecodeLimit))
+print("Close: " + str(bytecodeLimit - closeDifferential))
 
-for fileName in os.listdir(buildPath):
+buildPath = "./build/contracts"
+ignorePrefixes = ["Mock", "I"] # mocks and interfaces
+ignoreSuffixes = ["Test.json", "Proxy.json"] # tests and proxies
+
+parsedNames = []
+for name in os.listdir(buildPath):
+  use = True
+  for prefix in ignorePrefixes:
+    if name.startswith(prefix):
+      use = False
+      break
+  for suffix in ignoreSuffixes:
+    if name.endswith(suffix):
+      use = False
+      break
+  if (use):
+    parsedNames.append(name)
+
+for fileName in sorted(parsedNames):
   with open(buildPath + '/' + fileName, 'r') as f:
     contractData = json.load(f)
-    contractLen = len(contractData["deployedBytecode"]) / 2
+    contractLen = len(contractData["deployedBytecode"])
+    result = fileName + " bytecode len of " + str(contractLen)
     if contractLen > bytecodeLimit:
-      print("\t" + fileName + " bytecode len of " + str(contractLen))
+      print(colored(result, 'red'))
+    elif contractLen > bytecodeLimit - closeDifferential:
+      print(colored(result, 'yellow'))
     else:
-      print(fileName + " bytecode len of " + str(contractLen))
+      print(colored(result, 'green'))
